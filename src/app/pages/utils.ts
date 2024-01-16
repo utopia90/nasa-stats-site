b76c1f0db818ad80cf099e311a86c41bc961eac0
sara90 <sbr.90@hotmail.com>
2024-01-17 00:01:53 +0100

    remove unecesary images

--- src/app/pages/utils.ts
+++ src/app/pages/utils.ts
@@ -1,29 +1,28 @@
+export  function getTotalMeteoritCountInYearsRange(yearRange: { min: number, max: number }) {
 
-export  function getTotalMeteoritCountInYearsRange(yearRange: {min: number, max: number}){
- 
-  async function getYearCount(year: number){
+  async function getYearCount(year: number) {
     const apiUrl = `https://data.nasa.gov/resource/gh4g-9sfh.json?$select=count(*)&$where=year='${year}-01-01T00:00:00.000'`;
 
     const res = await fetch(apiUrl)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
-   
-    const data =  await res.json()
-    const count = data[0]?.count   
+
+    const data = await res.json()
+    const count = data[0]?.count
 
     return Number(count)
   }
   let promises = []
 
-  for(let i = yearRange.min; i <= yearRange.max; i++){
-    const yearCount =   getYearCount(i)
+  for (let i = yearRange.min; i <= yearRange.max; i++) {
+    const yearCount = getYearCount(i)
     promises.push(yearCount)
   }
-     let data : number[] = Promise.all(promises).then((promise) => promise ) as unknown as number[]
+  let data: number[] = Promise.all(promises).then((promise) => promise) as unknown as number[]
 
-     return data
-   
+  return data
+
 
 }
 
--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -10,7 +10,7 @@ import DashBoard from '../components/DashBoard';
   const MAX_YEAR = MIN_YEAR + YEARS_RANGE
 
   const firstYearsRange = {min: MIN_YEAR, max: MAX_YEAR}
-  let yearsRange: number[] = getTotalMeteoritCountInYearsRange(firstYearsRange)
+  let yearsRange: number[] =  getTotalMeteoritCountInYearsRange(firstYearsRange)
 
   return (
     <DashBoard initialData={yearsRange} yearsRange={firstYearsRange}/>
