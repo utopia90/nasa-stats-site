5e5b554c6fc2237687e363d285c69945f96f7e89
sara90 <sbr.90@hotmail.com>
2024-01-17 17:39:38 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ import { redirect } from "next/navigation"
 export default async function Page() {
     const session = await getServerSession(options)
 
-    if (!session) {
-        redirect('/api/auth/signin?callbackUrl/server')
-    }
+  
 
     return (
         <Home/>
